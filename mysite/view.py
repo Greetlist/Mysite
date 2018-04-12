0e4a7ffdb924f6ab6d703322195f58b05c3c11a6
za-lishigang <lishigang@zhongan.io>
2018-04-12 19:01:16 +0800

    1.添加两个前端框架
    Signed-off-by: za-lishigang <lishigang@zhongan.io>

--- mysite/view.py
+++ mysite/view.py
@@ -6,5 +6,5 @@ import datetime
 def hello(request):
 	context  = {}
 	context['hello'] = datetime.datetime.now()
-	context['second'] = ['a', 'b', 'c', 'd']
+	context['second'] = ['a', 'b', 'c', 'd', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e']
 	return render(request, 'hello.html', context)
